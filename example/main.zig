2b37f67516df8efbaf5be56a293e1511a43cf694
fitzx86 <takacs.mate16@gmail.com>
2026-05-11 15:01:49 +0200

    feat: adding demo

--- example/main.zig
+++ example/main.zig
@@ -16,14 +16,42 @@ pub fn main() !void {
         rl.beginDrawing();
         rl.clearBackground(.black);
 
+        var buf: [128:0]u8 = [_:0]u8{0} ** 128;
         rlimgui.begin();
 
-        imgui.ImGui_SetNextWindowSize(.{ .x = 400, .y = 100 }, imgui.ImGuiCond_FirstUseEver);
-        imgui.ImGui_SetNextWindowPos(.{ .x = 50, .y = 50 }, imgui.ImGuiCond_FirstUseEver);
+        const t: f32 = @floatCast(rl.getTime());
+        const cam_x = @sin(t) * 5.0;
+        const cam_z = @cos(t) * 5.0;
 
-        _ = imgui.ImGui_Begin("Hello dearzig", null, 0);
-        imgui.ImGui_Text("It works!");
+        rl.beginMode3D(.{
+            .position = .{ .x = cam_x, .y = 2, .z = cam_z },
+            .target = .{ .x = 0, .y = 0, .z = 0 },
+            .up = .{ .x = 0, .y = 1, .z = 0 },
+            .fovy = 45,
+            .projection = .perspective,
+        });
+
+        rl.drawCubeV(.{ .x = 0, .y = 0, .z = 0 }, .{ .x = 1, .y = 1, .z = 1 }, .red);
+        rl.drawCubeWiresV(.{ .x = 0, .y = 0, .z = 0 }, .{ .x = 1, .y = 1, .z = 1 }, .white);
+
+        rl.endMode3D();
+
+        imgui.ImGui_SetNextWindowSize(.{ .x = 400, .y = 200 }, imgui.ImGuiCond_FirstUseEver);
+        _ = imgui.ImGui_Begin("dearzig demo", null, 0);
+
+        imgui.ImGui_Text("Dear ImGui + Raylib + Zig");
+        imgui.ImGui_Separator();
+        _ = imgui.ImGui_InputText("Type here", &buf, 128, 0);
+        imgui.ImGui_Text("FPS:");
+        imgui.ImGui_SameLine();
+        var fps_buf: [32]u8 = undefined;
+        const fps_str = std.fmt.bufPrintZ(&fps_buf, "{d}", .{rl.getFPS()}) catch "?";
+        imgui.ImGui_Text(fps_str);
+
         imgui.ImGui_End();
+
+        // show imgui demo window for full feature showcase
+        imgui.ImGui_ShowDemoWindow(null);
 
         rlimgui.end();
 
